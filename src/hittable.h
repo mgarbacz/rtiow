c61afd80f8d491d9729644b2773b25af4712b35e
mgarbacz <michal@garbacz.io>
2023-10-20 12:18:56 -0400

    Type out front-face tracking from 6.4

--- src/hittable.h
+++ src/hittable.h
@@ -8,6 +8,15 @@ class hit_record {
     point3 p;
     vec3 normal;
     double t;
+    bool front_face;
+
+    void set_face_normal(cont ray& r, const vec3& outward_normal) {
+      // Sets hit record normal vector
+      // NOTE: parameter `outward_normal` is assumed to have unit length
+
+      front_face = dot(r.direction(), outward_normal) < 0;
+      normal = front_face ? outward_normal : -outward_normal;
+    }
 };
 
 class hittable {
