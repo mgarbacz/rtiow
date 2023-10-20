f0136f8781881a0a4ab2640e695a6d8ed8ed3e8c
mgarbacz <michal@garbacz.io>
2023-10-19 21:21:47 -0400

    Use color.h and vec.h in main to do the same thing

--- main.cc
+++ main.cc
@@ -1,7 +1,9 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main() {
-
   // Image
   int image_width = 256;
   int image_height = 256;
@@ -13,15 +15,9 @@ int main() {
     std::clog << "\rScanlines remaining: " << (image_height - j) << ' '
       << std::flush;
     for (int i = 0; i < image_width; ++i) {
-      auto r = double(i) / (image_width - 1);
-      auto g = double(j) / (image_height - 1);
-      auto b = 0;
-
-      int ir = static_cast<int>(255.999 * r);
-      int ig = static_cast<int>(255.999 * g);
-      int ib = static_cast<int>(255.999 * b);
-
-      std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+      auto pixel_color =
+        color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+      write_color(std::cout, pixel_color);
     }
   }
 
