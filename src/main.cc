1a045a9113e9f734de42239665ce41b25a53a7aa
mgarbacz <michal@garbacz.io>
2023-10-23 14:48:42 -0400

    Test camera fov setting with two touching spheres

--- src/main.cc
+++ src/main.cc
@@ -13,20 +13,18 @@ using std::make_shared;
 int main() {
   hittable_list world;
 
-  auto mat_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-  auto mat_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-  auto mat_left   = make_shared<dielectric>(1.5);
-  auto mat_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+  auto R = cos(pi /4 );
 
-  world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, mat_ground));
-  world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, mat_center));
-  world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, mat_left));
-  world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),  -0.4, mat_left));
-  world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, mat_right));
+  auto material_left  = make_shared<lambertian>(color(0, 0, 1));
+  auto material_right = make_shared<lambertian>(color(1, 0, 0));
+
+  world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left));
+  world.add(make_shared<sphere>(point3( R, 0, -1), R, material_right));
 
   camera cam;
 
   cam.aspect_ratio      = 16.0 / 9.0;
+  cam.vfov              = 90;
   cam.image_width       = 400;
   cam.samples_per_pixel = 100;
   cam.max_depth         = 50;
