a92ec3cb6ac32e33cbf20df9fdda65eff956c728
mgarbacz <michal@garbacz.io>
2023-10-23 14:27:08 -0400

    Add Schlick approximation for reflectance

--- src/material.h
+++ src/material.h
@@ -79,9 +79,11 @@ class dielectric : public material {
       double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
 
       bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+      bool is_steep_theta =
+        reflectance(cos_theta, refraction_ratio) > random_double();
       vec3 direction;
 
-      if (cannot_refract)
+      if (cannot_refract || is_steep_theta)
         direction = reflect(unit_direction, rec.normal);
       else
         direction = refract(unit_direction, rec.normal, refraction_ratio);
@@ -91,7 +93,14 @@ class dielectric : public material {
     }
 
   private:
-    double ir; // Index of refraction  
+    double ir; // Index of refraction 
+
+    // Use Schlick's approximation for reflectance.
+    static double reflectance(double cosine, double ref_idx) {
+      auto r0 = (1-ref_idx) / (1+ref_idx);
+      r0 = r0*r0;
+      return r0 + (1-r0)*pow((1 - cosine), 5);
+    } 
 };
 
 #endif
