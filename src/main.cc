081347a60a92c780bfc13410bf1ea6fbc1821184
mgarbacz <michal@garbacz.io>
2023-10-23 15:10:39 -0400

    Add back the scene with 3 balls
    - polished metal right
    - lambert blue mid
    - hollow glass left

--- src/main.cc
+++ src/main.cc
@@ -13,13 +13,16 @@ using std::make_shared;
 int main() {
   hittable_list world;
 
-  auto R = cos(pi /4 );
+  auto mat_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+  auto mat_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+  auto mat_left   = make_shared<dielectric>(1.5);
+  auto mat_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
-  auto material_left  = make_shared<lambertian>(color(0, 0, 1));
-  auto material_right = make_shared<lambertian>(color(1, 0, 0));
-
-  world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left));
-  world.add(make_shared<sphere>(point3( R, 0, -1), R, material_right));
+  world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, mat_ground));
+  world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, mat_center));
+  world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, mat_left));
+  world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),  -0.4, mat_left));
+  world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, mat_right));
 
   camera cam;
 
