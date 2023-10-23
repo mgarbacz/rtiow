0df2e7bba6ac2610f30da086b1c9354bd07f16f6
mgarbacz <michal@garbacz.io>
2023-10-22 20:28:11 -0400

    Simple lambertian material class that always scatters

--- src/material.h
+++ src/material.h
@@ -17,4 +17,22 @@ class material {
                          ray& scattered) const = 0;
 };
 
+class lambertian : public material {
+  public:
+    lambertian(const color& a) : albedo(a) {}
+
+    bool scatter(const ray& r_in,
+                 const hit_record& rec,
+                 color& attenuation,
+                 ray& scattered) const override {
+      auto scatter_direction = rec.normal + random_unit_vector();
+      scattered = ray(rec.p, scatter_direction);
+      attenuation = albedo;
+      return true;
+    }
+  
+  private:
+    color albedo;
+};
+
 #endif
