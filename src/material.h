6ff66a1cd1cc8fa6ca1b07ffefa53fe0f70d9ca4
mgarbacz <michal@garbacz.io>
2023-10-22 20:39:58 -0400

    Add metal material that just reflects rays per 10.4

--- src/material.h
+++ src/material.h
@@ -31,8 +31,26 @@ class lambertian : public material {
       // Catch degenerate scatter direction
       if (scatter_direction.near_zero())
         scatter_direction = rec.normal;
-        
+
       scattered = ray(rec.p, scatter_direction);
+      attenuation = albedo;
+      return true;
+    }
+  
+  private:
+    color albedo;
+};
+
+class metal : public material {
+  public:
+    metal(const color& a) : albedo(a) {}
+
+    bool scatter(const ray& r_in,
+                 const hit_record& rec,
+                 color& attenuation,
+                 ray& scattered) const override {
+      vec3 recflected = reflect(unit_vector(r_in.direction()), rec.normal);
+      scattered = ray(rec.p, recflected);
       attenuation = albedo;
       return true;
     }
