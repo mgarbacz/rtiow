30c42d04b21eb55123b114448e1f1d0e606f7c8c
mgarbacz <michal@garbacz.io>
2023-10-23 14:17:04 -0400

    Change middle sphere to blue lambert, polish right

--- src/main.cc
+++ src/main.cc
@@ -14,9 +14,9 @@ int main() {
   hittable_list world;
 
   auto mat_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-  auto mat_center = make_shared<dielectric>(1.5);
+  auto mat_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
   auto mat_left   = make_shared<dielectric>(1.5);
-  auto mat_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+  auto mat_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
   world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, mat_ground));
   world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, mat_center));
