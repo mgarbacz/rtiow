6c5ec56d39fc2fdf29f9b423699572e1e9a83c9f
mgarbacz <michal@garbacz.io>
2023-10-22 21:03:55 -0400

    Use material to determine ray refraction

--- src/camera.h
+++ src/camera.h
@@ -4,6 +4,7 @@
 #include "color.h"
 #include "hittable.h"
 #include "interval.h"
+#include "material.h"
 #include "math_utils.h"
 #include "ray.h"
 #include "vec3.h"
@@ -101,8 +102,11 @@ class camera {
         return color(0, 0, 0);
 
       if (world.hit(r, interval(0.001, infinity), rec)) {
-        vec3 direction = rec.normal + random_unit_vector();
-        return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
+        ray scattered;
+        color attenuation;
+        if (rec.mat->scatter(r, rec, attenuation, scattered))
+          return attenuation * ray_color(scattered, depth-1, world);
+        return color(0, 0, 0);
       }
 
       vec3 unit_direction = unit_vector(r.direction());
